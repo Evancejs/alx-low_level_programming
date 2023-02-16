101014695add8527d514fc462c7ee8610eedf55a
Evancejs <evancejsbanda@gmail.com>
2023-02-16 06:08:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main() prints using puts function
- */
-int main(void) 
+* main() - prints out given input using the puts()
+(* stdio.h:  C standard library that contains declarations for standard input/output operations)*
+* Return: this function returns 0 as instructed
+*/
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
