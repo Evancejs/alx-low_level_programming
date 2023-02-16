63f4f0791dd5018f3a128e2a18ece4919bd69d0b
Evancejs <evancejsbanda@gmail.com>
2023-02-16 05:51:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main() prints using puts function
+ */
 int main(void) 
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
