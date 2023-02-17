2b6bf372b16bb838093f44e333e7b74a444bb188
Evancejs <evancejsbanda@gmail.com>
2023-02-17 10:15:21 -0800

    print alphabet using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 /**
-* main - prints the alphabet, followed by a new line 
+* main - prints the alphabet, followed by a new line
 *
 * Return: 0
 */
 int main(void)
 {
 	int letter = 'a';
+		
 	while (letter <= 'z')
 	{
 	putchar(letter);
 	letter++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
