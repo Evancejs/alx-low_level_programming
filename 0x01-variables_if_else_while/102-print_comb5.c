5f035f389c7fe4e36e99b3f7e4f45123679cfe31
Evancejs <evancejsbanda@gmail.com>
2023-02-20 08:39:38 -0800

    102-print_com5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - print possible different combinations of 2 2 digits
+*
+* Return: 0
+*/
+int main()
+{
+	int i = 0;
+	int j = 0;
+	while (i <= 99)
+	{
+		while (j <= 99)
+		{
+			if (i < j)
+			{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+			j++;
+		}
+	i++;
+	j = 0;
+	}
+	return 0;
+}
