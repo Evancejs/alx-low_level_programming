42cee906c03508133ac44129656186ea06936586
Evancejs <evancejsbanda@gmail.com>
2023-02-23 07:12:46 -0800

    check upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*_isupper - checks for uppercase character
-*@c - takes the value for testing
+* _isupper - checks for uppercase character
+* @c: takes the value for testing
 *
-*return: 1 if uppercase else 0
+* Return: 1 if uppercase else 0
 */
 
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
