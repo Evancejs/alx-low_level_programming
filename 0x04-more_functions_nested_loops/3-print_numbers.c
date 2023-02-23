42569ab55a7f6f4c5df1a5ca8ef2fa8f7fa47f8b
Evancejs <evancejsbanda@gmail.com>
2023-02-23 08:19:56 -0800

    print 0-9 follwed by new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers - prints 0-9
+*
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
