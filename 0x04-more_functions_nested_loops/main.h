fb96138f7934f10709d747175b9ed2050d1cd0a2
Evancejs <evancejsbanda@gmail.com>
2023-02-23 10:30:41 -0800

    print 0-9 follwed by new line

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 
 int _putchar(char c);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
