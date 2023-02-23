a8a21d1980f082f70c94de9699ebf0a1138ab3fe
Evancejs <evancejsbanda@gmail.com>
2023-02-23 07:44:26 -0800

    finds product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - checks if input is 0-9
+* @a: inputs first value
+* @b: inputs second value
+*
+* Return: product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
