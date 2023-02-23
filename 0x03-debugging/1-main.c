ff96ada7df11273f06e99f1e471d16c2d617237a
Evancejs <evancejsbanda@gmail.com>
2023-02-23 05:37:17 -0800

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 		printf("Infinite loop incoming :(\n");
 
 		i = 0;
-
-		/**commmented while (i < 10)
-		*{
-		*	putchar(i);
-		}*/
-
+	/** while loop commented for causing an infinite loop
+	*	while (i < 10)
+	*	{
+	*			putchar(i);
+	*	}
+	*/
 		printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
