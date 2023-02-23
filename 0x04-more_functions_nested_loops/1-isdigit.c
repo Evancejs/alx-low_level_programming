6112af42195697d677829b841c5b1b5573c81a58
Evancejs <evancejsbanda@gmail.com>
2023-02-23 07:33:36 -0800

    check if its a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+* _isdigit - checks if input is 0-9
+* @c: inputs the value
+*
+* Return: 1 if true else
+*/
 
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
