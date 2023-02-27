e3850038d8d81b25b3a2d8abe531869226190df4
Evancejs <evancejsbanda@gmail.com>
2023-02-27 14:55:48 -0800

    string print

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	i = (len + 1) / 2;
 
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i += 2;
+		i++;
 	}
 	_putchar('\n');
 }
