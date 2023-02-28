16d42c894f9730d7e133489c18161eb5e1a94e9a
Evancejs <evancejsbanda@gmail.com>
2023-02-27 17:46:13 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* generate_password - generates a random valid password for the 101-crackme program
+*
+* Return: a string containing the generated password
+*/
+
+#define PASSWORD_LENGTH 10
+
+char *generate_password(void)
+{
+	static char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int charset_size = sizeof(charset) - 1;
+
+	srand(time(NULL));
+
+	int i = 0;
+
+	while (i < PASSWORD_LENGTH)
+	{
+		password[i] = charset[rand() % charset_size];
+		i++;
+	}
+
+	password[i] = '\0';
+
+	return (password);
+}
