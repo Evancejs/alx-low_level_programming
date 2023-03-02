1494bfc0e3a8f5f4044a21e2824bcff74d0d3d1a
Evancejs <evancejsbanda@gmail.com>
2023-03-02 05:06:03 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include "main.h"
+/**
+* leet - encodes a string into 1337
+* @str: gets input of the string
+*
+*Return: encoded str
+*/
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	char *leet_letters = "aAeEoOtTlL";
+	char *leet_codes = "4433007711";
+	char *leet_ptr;
+	char *code_ptr;
+
+	while (*ptr)
+	{
+		char *leet_ptr = leet_letters;
+		char *code_ptr = leet_codes;
+
+		while (*leet_ptr)
+		{
+			if (*ptr == *leet_ptr)
+			{
+				*ptr = *code_ptr;
+				break;
+			}
+			leet_ptr++;
+			code_ptr++;
+		}
+		ptr++;
+	}
+	return (str);
+}
