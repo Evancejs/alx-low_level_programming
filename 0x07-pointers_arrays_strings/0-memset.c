4dd72c5716a41f01352b2024dfec63017972a94c
Evancejs <evancejsbanda@gmail.com>
2023-03-06 15:01:52 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,15 @@
 *
 *Return: s
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
-		s[i] = b;
-		i++;
+	s[i] = b;
+	i++;
 	}
+
 	return (s);
 }
