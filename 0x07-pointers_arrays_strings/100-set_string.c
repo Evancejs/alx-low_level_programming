bb6ccf9d2e4144c3ea44613cd711a4ddd88d4ae9
Evancejs <evancejsbanda@gmail.com>
2023-03-07 04:18:01 -0800

    100-set-string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -9,12 +9,15 @@
  */
 void set_string(char **s, char *to)
 {
-	int i = 0;
+	size_t len = strlen(to);
+	char *new_str = malloc(len + 1);
 
-	while (*(to + i))
+	if (new_str == NULL)
 	{
-		*(*s + i) = *(to + i);
-		i++;
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(1);
 	}
-	*(*s + i) = *(to + i);
+	strcpy(new_str, to);
+	free(*s);
+	*s = new_str;
 }
