6e49190008dae3c51cdbe4b7d9b1adf7fe8a9c26
Evancejs <evancejsbanda@gmail.com>
2023-03-10 13:08:31 -0800

    a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -14,10 +14,27 @@ int main(int argc, char *argv[])
 {
 	char *program_name = argv[0];
 
+	(void)argc;
+
 	while (*program_name)
-		_putchar(*program_name++);
+	{
+		_putchar(*program_name);
+		program_name++;
+	}
 
 	_putchar('\n');
 
-	return (EXIT_SUCCESS);
+	return (0);
+}
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success, the number of characters written.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
 }
