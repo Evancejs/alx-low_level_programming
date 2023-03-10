aee6e3433b557364a1bc0420cdfc4642accd7016
Evancejs <evancejsbanda@gmail.com>
2023-03-10 14:21:59 -0800

    program that adds positive numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,81 +1,44 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include "main.h"
-/**
- * count_coins - Counts the minimum number
- * of coins needed to make change
- * @cents: The amount of money in cents
- *
- * Return: The minimum number of coins needed to make change
- */
-int count_coins(int cents)
-{
-	int coins = 0;
-
-	if (cents < 0)
-		return (0);
-
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
-	}
-
-	return (coins);
-}
 
 /**
  * main - Prints the minimum number of coins
- * needed to make change
- * @argc: The number of arguments passed to the program
- * @argv: An array of pointers to the arguments
+ * to make change for an amount of money
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
  * Return: 0 if successful, 1 if any argument is invalid
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
 
-	cents = atoi(argv[1]);
+	num = atoi(argv[1]);
+	result = 0;
 
-	coins = count_coins(cents);
+	if (num < 0)
+	{
+	printf("0\n");
+	return (0);
+	}
 
-	print_number(coins);
-	_putchar('\n');
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+		result++;
+		num -= coins[j];
+		}
+	}
 
+	printf("%d\n", result);
 	return (0);
 }
