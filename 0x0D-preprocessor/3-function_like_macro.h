dcd9aea1f7294669b5a294bf9dccadb8bf65ed9b
Evancejs <evancejsbanda@gmail.com>
2023-03-20 11:29:51 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/* Macro to compute the absolute value of a number */
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+/* Macro to compute the minimum of two numbers */
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+/* Macro to compute the maximum of two numbers */
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
